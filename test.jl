974ffe206f3d072b436b4a72a2058c532a0cbe91
Kevin <kmazza2@uic.edu>
2025-02-23 12:37:46 -0600

    Add test

--- test.jl
+++ test.jl
@@ -3,7 +3,14 @@ import .OptimalDesign as OD
 
 standard_basis = Float64.([1 0 0; 0 1 0; 0 0 1])
 e3 = Float64.([0 0 1]')
-B = OD.orthogonal_subspace_basis(
+A = OD.orthogonal_subspace_basis(
     standard_basis, e3, 1e-5
 )
-@assert size(B, 2) == 2
+@assert size(A, 2) == 2
+
+standard_basis = Float64.([1 0 0 0; 0 1 0 0; 0 0 1 0; 0 0 0 1])
+e1 = Float64.([1 0 0 0]')
+B = OD.orthogonal_subspace_basis(
+    standard_basis, e1, 1e-5
+)
+@assert size(B, 2) == 3
